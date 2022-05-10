26bd56460e643003f833f4ffb2c4295683d14fd0
Jesus Nieto <nietopino@gmail.com>
2022-05-10 16:15:04 +0200

    working api

--- apirest/src/main/java/com/tfg/apirest/APIController.java
+++ apirest/src/main/java/com/tfg/apirest/APIController.java
@@ -4,9 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 
 public class APIController {
+
 
     @GetMapping("/")
     public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
